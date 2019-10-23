6af5549ddff0a3ce42e6125fce54b6734a8e1bd3
arham09 <arham.abiyan@gmail.com>
2019-10-23 17:31:34 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const server = express();
+
+server.get(`/`, (req, res) => {
+  res.send(`Hello World !!`);
+});
+
+server.listen(process.env.PORT || 6969);
+
+module.exports = server;
